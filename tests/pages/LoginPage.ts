ece003f93edf640b571d36257cb6d37358ecfaf0
Patryk Grund <119923185+Pattrick97@users.noreply.github.com>
2025-08-06 20:35:59 +0200

    Fixed login test cases

--- tests/pages/LoginPage.ts
+++ tests/pages/LoginPage.ts
@@ -1,19 +1,47 @@
-import { Page } from "@playwright/test";
+import { Page, expect } from "@playwright/test";
 
 export class LoginPage {
   constructor(private page: Page) {}
 
+  async handleCookiePopupIfVisible() {
+    const consentModal = this.page.locator(
+      "text=This site asks for consent to use your data"
+    );
+    const consentButton = this.page.locator('button:has-text("Consent")');
+
+    if (await consentModal.isVisible()) {
+      console.log("Consent modal is visible");
+
+      await expect(consentButton).toBeVisible({ timeout: 5000 });
+      await expect(consentButton).toBeEnabled();
+
+      await consentButton.click();
+      console.log("Clicked Consent button");
+    }
+  }
+
   async navigate() {
-    await this.page.goto("/login");
+    await this.page.goto(`${process.env.BASE_URL}/login`);
+    await this.handleCookiePopupIfVisible();
+    await expect(
+      this.page.locator('h2:has-text("Login to your account")')
+    ).toBeVisible();
   }
 
   async login(email: string, password: string) {
+    await this.handleCookiePopupIfVisible();
     await this.page.fill('input[data-qa="login-email"]', email);
     await this.page.fill('input[data-qa="login-password"]', password);
     await this.page.click('button[data-qa="login-button"]');
   }
 
+  async assertSuccessfulLogin() {
+    await expect(this.page.locator('a[href="/logout"]')).toBeVisible();
+  }
+
   async assertLoginError() {
-    await this.page.waitForSelector(".login-form .error");
+    await expect(
+      this.page.locator('p:has-text("Your email or password is incorrect!")')
+    ).toBeVisible();
   }
 }
--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -1,10 +1,23 @@
-// tests/ui/login.spec.ts
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 import { LoginPage } from "../pages/LoginPage";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-test("Login with invalid data", async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.navigate();
-  await loginPage.login("fake@example.com", "wrongpassword");
-  await loginPage.assertLoginError();
+test.describe("Login tests", () => {
+  test(" @smoke Login with valid credentials", async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.navigate();
+    await loginPage.login(
+      process.env.VALID_EMAIL!,
+      process.env.VALID_PASSWORD!
+    );
+    await loginPage.assertSuccessfulLogin();
+  });
+
+  test("Login with incorrect password", async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.navigate();
+    await loginPage.login(process.env.VALID_EMAIL!, "wrongPassword123");
+    await loginPage.assertLoginError();
+  });
 });
